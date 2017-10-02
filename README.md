e3f87838abc210bebb7193483d241889d422514c
Nik Latkin <nlatkin@nextradioapp.com>
2017-10-02 16:12:02 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Pixel Art Maker Project
+
+## Table of Contents
+
+* [Instructions](#instructions)
+* [About](#about)
+
+## Instructions
+
+To get started to play you need to download game folder on your machine and then run index.html to start the game.
+
+## About
+
+This is a simple canvas written in vanila JS for making pixel art, you've got your grid and color picker - that's it. This project made  by me for Udacity Front End Developer Nanodegree program
---
+++ designs.js
@@ -0,0 +1,38 @@
+//Program defaults
+const tBody = document.createElement("tBody");
+const table = document.getElementById("pixel_canvas");
+// table.appendChild(tBody);
+const colorPicker = document.getElementById("colorPicker");
+
+const makeGrid = (height, width) => {
+
+// Your code goes here!
+  //Clear the table body
+  while(table.rows.length > 0) {
+    table.deleteRow(0)
+  };
+  //Create cells to insert
+  for(let i = 0; i < height; i++) {
+    const row = table.insertRow(i);
+    for(let j = 0; j < width; j++) {
+      const cell = row.insertCell(j);
+      cell.onclick = () => {
+        cell.style.backgroundColor = colorPicker.value;
+      };
+    }
+  }
+}
+
+//Submit-button event listener
+const sizePicker = document.querySelector("#sizePicker");
+sizePicker.addEventListener("submit", function(event){
+  event.preventDefault();
+  const inputHeight = document.getElementById("input_height").value;
+  const inputWidth = document.getElementById("input_width").value;
+  makeGrid(inputHeight, inputWidth);
+});
+
+//Fill td with customColor
+document.getElementById("pixel_canvas").addEventListener("click", (event) => {
+  event.target.style.background = colorPicker.value;
+}, false);
